7821aecbc0ac1043fd502f0e756707b62a34bc5a
raunak2004 <raunak.kanhere@gmail.com>
2017-01-11 14:20:05 -0500

    changes for sql server

--- server.js
+++ server.js
@@ -5,25 +5,25 @@
 var express = require('express'),
     routes = require('./src/routes'),
     user = require('./src/routes/user'),
+    taskroute = require('./src/routes/taskroute'),
     http = require('http'),
     path = require('path'),
     sql = require('mssql');
 
-//var config = {
-//    user: 'raunak2004',
-//    password: 'Kelmai1986',
-//    server: 'nodeexpressraunak.database.windows.net',
-//    database: 'Task',
-//    options: {
-//        encrypt: true
-//    }
-//};
-//
-//sql.connect(config, function (err) {
-//    console.log(err);
-//});
+var config = {
+    user: 'raunak2004',
+    password: 'Kelmai1986',
+    server: 'nodeexpressraunak.database.windows.net',
+    database: 'Task',
+    options: {
+        encrypt: true
+    }
+};
 
-var taskRouter = require('./src/routes/taskroute');
+sql.connect(config, function (err) {
+    console.log(err);
+});
+
 var app = express();
 
 app.configure(function () {
@@ -36,7 +36,6 @@ app.configure(function () {
     app.use(express.methodOverride());
     app.use(app.router);
     app.use(express.static(path.join(__dirname, 'public')));
-    app.use('/Task', taskRouter);
 });
 
 app.configure('development', function () {
@@ -45,6 +44,7 @@ app.configure('development', function () {
 
 app.get('/', routes.index);
 app.get('/users', user.list);
+app.get('/task', taskroute.task);
 
 http.createServer(app).listen(app.get('port'), function () {
     console.log("Express server listening on port " + app.get('port'));
