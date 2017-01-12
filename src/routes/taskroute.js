8674437fc4bd95bfc146e39eaa0ab2d011a58e0a
Raunak <raunak.kanhere@gmail.com>
2017-01-11 23:18:34 -0500

    mnk

--- src/routes/taskroute.js
+++ src/routes/taskroute.js
@@ -5,7 +5,7 @@ var sql = require('mssql');
 exports.createtasktable = function (req, res) {
     var table = new sql.Table('Task'); // or temporary table, e.g. #temptable 
     table.create = true;
-    table.columns.add('ID', sql.Guid, {
+    table.columns.add('ID', sql.guid, {
         nullable: false,
         primary: true
     });
