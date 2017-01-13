1b4b8777bc20944bdf29ded1f27bdc309793fac9
raunak2004 <raunak.kanhere@gmail.com>
2017-01-13 17:13:51 -0500

    reverting passport

--- src/config/passport.js
+++ src/config/passport.js
@@ -21,32 +21,32 @@ var userPassport = function (app) {
     //        });
     //    }));
 
-        passport.use(new WindowsStrategy({
-            integrated: true
-        }, function (profile, done) {
-            var user = {
-                id: profile.id,
-            };
-            done(null, user);
-        }));
-
-    app.use(passport.session());
-
-    passport.serializeUser(function (user, callback) {
-        console.log(user);
-        callback(null, user);
-    });
-
-    passport.deserializeUser(function (userId, callback) {
-        console.log(user);
-        callback(null, user);
-    });
-
-    app.get('/express-passport',
-        passport.authenticate('WindowsAuthentication'),
-        function (req, res) {
-            res.json(req.user);
-        });
+//        passport.use(new WindowsStrategy({
+//            integrated: true
+//        }, function (profile, done) {
+//            var user = {
+//                id: profile.id,
+//            };
+//            done(null, user);
+//        }));
+//
+//    app.use(passport.session());
+//
+//    passport.serializeUser(function (user, callback) {
+//        console.log(user);
+//        callback(null, user);
+//    });
+//
+//    passport.deserializeUser(function (userId, callback) {
+//        console.log(user);
+//        callback(null, user);
+//    });
+//
+//    app.get('/express-passport',
+//        passport.authenticate('WindowsAuthentication'),
+//        function (req, res) {
+//            res.json(req.user);
+//        });
 };
 
 module.exports = userPassport;
